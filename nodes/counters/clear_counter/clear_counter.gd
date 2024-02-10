a573e3a63300afe16413de85f35a134903a17db9
Mikhail Molodozhenov (Mishamol) <mishamol@bk.ru>
2024-02-10 14:44:50 +0300

    Kitchen Object Parent
    https://youtu.be/AmGSEH7QcDg?t=13067

--- nodes/counters/clear_counter/clear_counter.gd
+++ nodes/counters/clear_counter/clear_counter.gd
@@ -5,7 +5,17 @@ extends StaticBody3D
 @export var _kitchen_object_res: KitchenObjectRes
 @export var _counter_top_point: Node3D
 
-var _kitchen_object: KitchenObject;
+@export var _second_clear_counter: ClearCounter
+@export var _testing: bool = false
+
+var _kitchen_object: KitchenObject
+
+
+func _input(event: InputEvent) -> void:
+	if _testing && event.is_action_pressed("test"):
+		print("test")
+		if _kitchen_object != null:
+			_kitchen_object.set_clear_counter(_second_clear_counter)
 
 
 func interact():
@@ -13,7 +23,26 @@ func interact():
 		var kitchen_object_instance = load(_kitchen_object_res.node).instantiate()
 		_counter_top_point.add_child(kitchen_object_instance)
 		
-		_kitchen_object = kitchen_object_instance
-		_kitchen_object.set_clear_counter(self)
+		kitchen_object_instance.set_clear_counter(self)
 	else:
 		print(_kitchen_object.get_clear_counter())
+
+
+func get_counter_top_point() -> Node3D:
+	return _counter_top_point
+
+
+func set_kitchen_object(kitchen_object: KitchenObject):
+	_kitchen_object = kitchen_object
+	
+
+func get_kitchen_object() -> KitchenObject:
+	return _kitchen_object
+	
+	
+func clear_kitchen_object():
+	_kitchen_object = null
+	
+	
+func has_kitchen_object() -> bool:
+	return _kitchen_object != null
--- nodes/kitchen_objects/kitchen_object.gd
+++ nodes/kitchen_objects/kitchen_object.gd
@@ -11,8 +11,21 @@ var _clear_counter: ClearCounter
 func get_resource() -> KitchenObjectRes:
 	return _kitchen_object_res
 
+
 func set_clear_counter(clear_counter: ClearCounter):
+	if _clear_counter != null:
+		_clear_counter.clear_kitchen_object()
+	
 	_clear_counter = clear_counter
+	
+	if clear_counter.has_kitchen_object():
+		printerr("Counter already has a Kitchen Object!")
+	
+	clear_counter.set_kitchen_object(self)
+	
+	reparent(clear_counter.get_counter_top_point())
+	position = Vector3.ZERO
+
 
 func get_clear_counter() -> ClearCounter:
 	return _clear_counter
