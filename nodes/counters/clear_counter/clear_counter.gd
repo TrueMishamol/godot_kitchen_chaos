a37752b48dc88a5048090efae6720f13276c66f1
Mikhail Molodozhenov (Mishamol) <mishamol@bk.ru>
2024-02-14 15:59:23 +0300

    Clean up

--- nodes/counters/clear_counter/clear_counter.gd
+++ nodes/counters/clear_counter/clear_counter.gd
@@ -2,12 +2,8 @@ class_name ClearCounter
 extends StaticBody3D
 
 
-@export var kitchen_object_parent: KitchenObjectParent
-@export var _counter_top_point: Node3D
-
-
+@export var _kitchen_object_parent: KitchenObjectParent
 @export var _kitchen_object_res: KitchenObjectRes
-
 @export var _second_clear_counter: ClearCounter
 @export var _testing: bool = false
 	
@@ -15,35 +11,19 @@ extends StaticBody3D
 func _input(event: InputEvent) -> void:
 	if _testing && event.is_action_pressed("test"):
 		print("test")
-		if kitchen_object_parent.kitchen_object != null:
-			kitchen_object_parent.kitchen_object.set_kitchen_object_parent(_second_clear_counter.kitchen_object_parent)
+		if _kitchen_object_parent.has_kitchen_object():
+			_kitchen_object_parent.get_kitchen_object().set_kitchen_object_parent(_second_clear_counter._kitchen_object_parent)
 
 
 func interact(new_kitchen_object_parent: KitchenObjectParent):
-	if kitchen_object_parent.kitchen_object == null:
+	if !_kitchen_object_parent.has_kitchen_object():
 		var kitchen_object_instance: KitchenObject = load(_kitchen_object_res.node).instantiate()
-		_counter_top_point.add_child(kitchen_object_instance)
+		_kitchen_object_parent.add_child(kitchen_object_instance)
 		
-		kitchen_object_instance.set_kitchen_object_parent(kitchen_object_parent)
+		kitchen_object_instance.set_kitchen_object_parent(_kitchen_object_parent)
 	else:
-		kitchen_object_parent.kitchen_object.set_kitchen_object_parent(new_kitchen_object_parent)
+		_kitchen_object_parent.get_kitchen_object().set_kitchen_object_parent(new_kitchen_object_parent)
 
 
-func get_counter_top_point() -> Node3D:
-	return _counter_top_point
-
-
-func set_kitchen_object(kitchen_object: KitchenObject):
-	kitchen_object_parent.kitchen_object = kitchen_object
-	
-
-func get_kitchen_object() -> KitchenObject:
-	return kitchen_object_parent.kitchen_object
-	
-	
-func clear_kitchen_object():
-	kitchen_object_parent.kitchen_object = null
-	
-	
-func has_kitchen_object() -> bool:
-	return kitchen_object_parent.kitchen_object != null
+func get_kitchen_object_parent() -> Node3D:
+	return _kitchen_object_parent
--- nodes/kitchen_object_parent.gd
+++ nodes/kitchen_object_parent.gd
@@ -2,20 +2,20 @@ class_name KitchenObjectParent
 extends Node3D
 
 
-var kitchen_object: KitchenObject
+var _kitchen_object: KitchenObject
 
 
 func set_kitchen_object(new_kitchen_object: KitchenObject):
-	kitchen_object = new_kitchen_object
+	_kitchen_object = new_kitchen_object
 	
 
 func get_kitchen_object() -> KitchenObject:
-	return kitchen_object
+	return _kitchen_object
 	
 	
 func clear_kitchen_object():
-	kitchen_object = null
+	_kitchen_object = null
 	
 	
 func has_kitchen_object() -> bool:
-	return kitchen_object != null
+	return _kitchen_object != null
